3d4b2c3cc2cf686da9f116907b468ed455a75981
Kishan Sagathiya <kishansagathiya@gmail.com>
2018-09-21 16:34:09 +0530

    Create pomodoro.go

---
+++ pomodoro.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+	"time"
+)
+
+/*
+USAGE: go run pomodoro.go <number of minutes>
+
+SAMPLE:
+[kishansagathiya@localhost utility]$ go run pomodoro.go 60
+Running pomodoro for 60 minutes
+59
+58
+*/
+func main() {
+
+	argsWithProg := os.Args
+	i, err := strconv.Atoi(argsWithProg[1])
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("Running pomodoro for %d minutes\n", i)
+
+	for i > 0 {
+		time.Sleep(1 * time.Minute)
+		i--
+		fmt.Println(i)
+	}
+
+	err = exec.Command("vlc", "alarm.mp3").Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
